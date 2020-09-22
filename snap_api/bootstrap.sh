ababc3c159fbbf9af54d4c1b2fe44f8e86298b68
jd10ne <18096052+jd10ne@users.noreply.github.com>
2020-09-22 21:18:42 +0900

    update: change threads

--- snap_api/bootstrap.sh
+++ snap_api/bootstrap.sh
@@ -4,4 +4,4 @@
 source $(pipenv --venv)/bin/activate
 
 # flask run -h 0.0.0.0 -p $PORT
-gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 index:app
+gunicorn --bind :$PORT --workers 1 --threads 1  --timeout 0 index:app
